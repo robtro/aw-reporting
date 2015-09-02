cab69e1be0ae3c213a47124aa51d38ecac98209b
Allison Greene <allig@google.com>
2015-09-02 09:34:34 -0400

    Initial version of final url report

---
+++ aw-reporting-model/src/main/java/com/google/api/ads/adwords/awreporting/model/entities/ReportFinalUrl.java
@@ -0,0 +1,253 @@
+// Copyright 2011 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.api.ads.adwords.awreporting.model.entities;
+
+import com.google.api.ads.adwords.awreporting.model.csv.annotation.CsvField;
+import com.google.api.ads.adwords.awreporting.model.csv.annotation.CsvReport;
+import com.google.api.ads.adwords.awreporting.model.csv.annotation.MoneyField;
+import com.google.api.ads.adwords.awreporting.model.util.BigDecimalUtil;
+import com.google.api.ads.adwords.awreporting.model.util.UrlHashUtil;
+import com.google.api.ads.adwords.lib.jaxb.v201506.ReportDefinitionReportType;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Specific report class for ReportFinalUrl
+ *
+ * @author allig@google.com (Allison Greene)
+ */
+@Entity
+@com.googlecode.objectify.annotation.Entity
+@Table(name = "AW_ReportFinalUrl")
+@CsvReport(value = ReportDefinitionReportType.FINAL_URL_REPORT)
+public class ReportFinalUrl extends ReportBase {
+  
+  @Column(name = "ACTIVE_VIEW_CPM")
+  @CsvField(value = "Active View avg. CPM", reportField = "ActiveViewCpm")
+  //@MoneyField
+  private BigDecimal activeViewCpm;
+
+  @Column(name = "ACTIVE_VIEW_IMPRESSIONS")
+  @CsvField(value = "Active View viewable impressions", reportField = "ActiveViewImpressions")
+  private Long activeViewImpressions;
+  
+  @Column(name = "AD_GROUP_ID")
+  @CsvField(value = "Ad group ID", reportField = "AdGroupId")
+  private Long adGroupId;
+  
+  @Column(name = "AD_GROUP_NAME")
+  @CsvField(value = "Ad group", reportField = "AdGroupName")
+  private String adGroupName;
+
+  @Column(name = "AD_GROUP_STATUS")
+  @CsvField(value = "Ad group state", reportField = "AdGroupStatus")
+  private String adGroupStatus;
+  
+  @Column(name = "CAMPAIGN_ID")
+  @CsvField(value = "Campaign ID", reportField = "CampaignId")
+  private Long campaignId;
+  
+  @Column(name = "CAMPAIGN_NAME")
+  @CsvField(value = "Campaign", reportField = "CampaignName")
+  private String campaignName;
+
+  @Column(name = "CAMPAIGN_STATUS")
+  @CsvField(value = "Campaign state", reportField = "CampaignStatus")
+  private String campaignStatus;
+
+  @Column(name = "CONVERSION_TRACKER_ID")
+  @CsvField(value = "Conversion Tracker Id", reportField = "ConversionTrackerId")
+  private Long conversionTrackerId;
+  
+  @Column(name = "CRITERIA_PARAMETERS")
+  @CsvField(value = "Keyword / Placement", reportField = "CriteriaParameters")
+  private String criteriaParameters;
+
+  @Column(name = "CRITERIA_TYPE_NAME")
+  @CsvField(value = "Match type", reportField = "CriteriaTypeName")
+  private String criteriaTypeName;
+  
+  @Column(name = "EFFECTIVE_FINAL_URL", length = 2048)
+  @CsvField(value = "Final URL", reportField = "EffectiveFinalUrl")
+  private String effectiveFinalUrl;
+
+  @Column(name = "SLOT")
+  @CsvField(value = "Top vs. Other", reportField = "Slot")
+  private String slot;
+  
+  /**
+   * Hibernate needs an empty constructor
+   */
+  public ReportFinalUrl() {
+  }
+
+  public ReportFinalUrl(Long topAccountId, Long accountId) {
+    this.topAccountId = topAccountId;
+    this.accountId = accountId;
+  }
+
+  @Override
+  public void setId() {
+    // Generating unique id after having accountId, campaignId, adGroupId and date
+    this.id = "";
+    if (this.getAccountId() != null) {
+      this.id += this.getAccountId() + "-";
+    }
+    if (this.getCampaignId() != null) {
+      this.id += this.getCampaignId() + "-";
+    }
+    if (this.getAdGroupId() != null) {
+      this.id += this.getAdGroupId() + "-";
+    }
+    if (this.getCriteriaParameters() != null) { 
+      this.id += this.getCriteriaParameters() + "-";
+    }
+ 
+    // Generating a SHA-1 Hash of the URLs for ID generation
+    if (this.getEffectiveFinalUrl() != null) { 
+      this.id += UrlHashUtil.createUrlHash(this.getEffectiveFinalUrl());
+    }
+
+    this.id += setIdDates();
+
+    // Adding extra fields for unique ID
+    if (this.getAdNetwork() != null && this.getAdNetwork().length() > 0) {
+      this.id += "-" + this.getAdNetwork();
+    }
+    if (this.getAdNetworkPartners() != null && this.getAdNetworkPartners().length() > 0) {
+      this.id += "-" + this.getAdNetworkPartners();
+    }
+    if (this.getDevice() != null && this.getDevice().length() > 0) {
+      this.id += "-" + this.getDevice();
+    }
+    if (this.getClickType() != null && this.getClickType().length() > 0) {
+      this.id += "-" + this.getClickType();
+    }
+
+  }
+
+  public String getActiveViewCpm() {
+    return BigDecimalUtil.formatAsReadable(activeViewCpm);
+  }
+
+  public BigDecimal getActiveViewCpmBigDecimal() {
+    return activeViewCpm;
+  }
+
+  public void setActiveViewCpm(String activeViewCpm) {
+    this.activeViewCpm = BigDecimalUtil.parseFromNumberStringPercentage(activeViewCpm);
+  }
+  
+  public Long getActiveViewImpressions() {
+    return activeViewImpressions;
+  }
+  
+  public void setActiveViewImpressions(Long activeViewImpressions) {
+    this.activeViewImpressions = activeViewImpressions;
+  }
+  
+  public Long getAdGroupId() {
+    return adGroupId;
+  }
+
+  public void setAdGroupId(Long adGroupId) {
+    this.adGroupId = adGroupId;
+  }
+
+  public String getAdGroupName() {
+    return adGroupName;
+  }
+
+  public void setAdGroupName(String adGroupName) {
+    this.adGroupName = adGroupName;
+  }
+
+  public String getAdGroupStatus() {
+    return adGroupStatus;
+  }
+
+  public void setAdGroupStatus(String adGroupStatus) {
+    this.adGroupStatus = adGroupStatus;
+  }
+
+  public Long getCampaignId() {
+    return campaignId;
+  }
+
+  public void setCampaignId(Long campaignId) {
+    this.campaignId = campaignId;
+  }
+
+  public String getCampaignName() {
+    return campaignName;
+  }
+
+  public void setCampaignName(String campaignName) {
+    this.campaignName = campaignName;
+  }
+
+  public String getCampaignStatus() {
+    return campaignStatus;
+  }
+
+  public void setCampaignStatus(String campaignStatus) {
+    this.campaignStatus = campaignStatus;
+  }
+
+  public Long getConversionTrackerId() {
+    return conversionTrackerId;
+  }
+  
+  public void setConversionTrackerId(Long conversionTrackerId) {
+    this.conversionTrackerId = conversionTrackerId;
+  }
+  
+  public String getCriteriaParameters() {
+    return criteriaParameters;
+  }
+
+  public void setCriteriaParameters(String criteriaParameters) {
+    this.criteriaParameters = criteriaParameters;
+  }
+
+  public String getCriteriaTypeName() {
+    return criteriaTypeName;
+  }
+
+  public void setCriteriaTypeName(String criteriaTypeName) {
+    this.criteriaTypeName = criteriaTypeName;
+  }
+
+  public String getEffectiveFinalUrl() {
+    return effectiveFinalUrl;
+  }
+
+  public void setEffectiveFinalUrl(String effectiveFinalUrl) {
+    this.effectiveFinalUrl = effectiveFinalUrl;
+  }
+
+  public String getSlot() {
+    return slot;
+  }
+
+  public void setSlot(String slot) {
+    this.slot = slot;
+  }
+  
+}
---
+++ aw-reporting-model/src/test/java/com/google/api/ads/adwords/awreporting/model/definitions/ReportFinalUrlDefinitionTest.java
@@ -0,0 +1,217 @@
+// Copyright 2013 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.api.ads.adwords.awreporting.model.definitions;
+
+import com.google.api.ads.adwords.awreporting.model.entities.ReportFinalUrl;
+import com.google.api.ads.adwords.lib.jaxb.v201506.ReportDefinitionReportType;
+
+import junit.framework.Assert;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Tests the Ad Performance report definition.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:aw-report-model-test-beans.xml")
+public class ReportFinalUrlDefinitionTest
+extends AbstractReportDefinitionTest<ReportFinalUrl> {
+
+  /**
+   * C'tor
+   */
+  public ReportFinalUrlDefinitionTest() {
+    super(ReportFinalUrl.class, ReportDefinitionReportType.FINAL_URL_REPORT,
+        "src/test/resources/csv/final-url.csv");
+  }
+
+  /**
+   * @see com.google.api.ads.adwords.awreporting.model.definitions.
+   * AbstractReportDefinitionTest#testFirstEntry(
+   * com.google.api.ads.adwords.awreporting.model.entities.Report)
+   */
+  @Override
+  protected void testFirstEntry(ReportFinalUrl row) {
+
+    Assert.assertEquals(123456789L, row.getCampaignId().longValue());
+    Assert.assertEquals("Campaign1", row.getCampaignName());
+    Assert.assertEquals("enabled", row.getCampaignStatus());
+    Assert.assertEquals(100000000L, row.getAdGroupId().longValue());
+    Assert.assertEquals("AdGroup1" , row.getAdGroupName());
+    Assert.assertEquals("enabled", row.getAdGroupStatus());
+    
+    Assert.assertEquals(0.00, row.getActiveViewCpmBigDecimal().doubleValue());
+    Assert.assertEquals(25L, row.getActiveViewImpressions().longValue());
+    Assert.assertEquals("Display Network", row.getAdNetwork());
+    Assert.assertEquals("Display Network", row.getAdNetworkPartners());
+    
+    Assert.assertEquals(0.00, row.getAvgCpc().doubleValue());
+    Assert.assertEquals(0.00, row.getAvgCpm().doubleValue());
+    Assert.assertEquals(1.9, row.getAvgPositionBigDecimal().doubleValue());
+    
+    Assert.assertEquals(0L, row.getClicks().longValue());
+    Assert.assertEquals("Headline", row.getClickType());
+    
+    Assert.assertEquals(0.00, row.getConversionRateManyPerClickBigDecimal().doubleValue());
+    Assert.assertEquals(0L, row.getConversionsManyPerClick().longValue());
+    Assert.assertEquals(0.00, row.getConversionValueBigDecimal().doubleValue());
+    
+    Assert.assertEquals(0.00, row.getCost().doubleValue());
+    Assert.assertEquals(0.00, row.getCostPerConversionManyPerClick().doubleValue());
+    Assert.assertEquals("Content", row.getCriteriaParameters());
+    Assert.assertEquals("Broad", row.getCriteriaTypeName());
+    
+    Assert.assertEquals("2015-08-25", row.getDay());
+    Assert.assertEquals("Tuesday", row.getDayOfWeek());
+    Assert.assertEquals("Mobile devices with full browsers", row.getDevice());
+    Assert.assertEquals("https://example.com/index/type/P/", row.getEffectiveFinalUrl());
+    
+    Assert.assertEquals(66L, row.getImpressions().longValue());
+    Assert.assertEquals("2015-08-01", row.getMonth());
+    Assert.assertEquals("August", row.getMonthOfYear());
+    Assert.assertEquals("2015-07-01", row.getQuarter());
+    
+    Assert.assertEquals("Google Display Network", row.getSlot());
+    Assert.assertEquals(0.00, row.getValuePerConversionManyPerClickBigDecimal().doubleValue());
+    Assert.assertEquals("2015-08-24", row.getWeek());
+    Assert.assertEquals(2015L, row.getYear().longValue());
+    
+  }
+
+  /**
+   * @see com.google.api.ads.adwords.awreporting.model.definitions.
+   * AbstractReportDefinitionTest#testLastEntry(
+   * com.google.api.ads.adwords.awreporting.model.entities.Report)
+   */
+  @Override
+  protected void testLastEntry(ReportFinalUrl row) {
+    Assert.assertEquals(123456789L, row.getCampaignId().longValue());
+    Assert.assertEquals("Campaign1", row.getCampaignName());
+    Assert.assertEquals("enabled", row.getCampaignStatus());
+    Assert.assertEquals(300000000L, row.getAdGroupId().longValue());
+    Assert.assertEquals("AdGroup3" , row.getAdGroupName());
+    Assert.assertEquals("enabled", row.getAdGroupStatus());
+    
+    //Assert.assertEquals(0.00, row.getActiveViewCpmBigDecimal().doubleValue());
+    Assert.assertEquals(1L, row.getActiveViewImpressions().longValue());
+    Assert.assertEquals("Display Network", row.getAdNetwork());
+    Assert.assertEquals("Display Network", row.getAdNetworkPartners());
+    
+    Assert.assertEquals(0.00, row.getAvgCpc().doubleValue());
+    Assert.assertEquals(0.00, row.getAvgCpm().doubleValue());
+    Assert.assertEquals(1.0, row.getAvgPositionBigDecimal().doubleValue());
+    
+    Assert.assertEquals(0L, row.getClicks().longValue());
+    Assert.assertEquals("Headline", row.getClickType());
+    
+    Assert.assertEquals(0.00, row.getConversionRateManyPerClickBigDecimal().doubleValue());
+    Assert.assertEquals(0L, row.getConversionsManyPerClick().longValue());
+    Assert.assertEquals(0.00, row.getConversionValueBigDecimal().doubleValue());
+    
+    Assert.assertEquals(0.00, row.getCost().doubleValue());
+    Assert.assertEquals(0.00, row.getCostPerConversionManyPerClick().doubleValue());
+    Assert.assertEquals("Content", row.getCriteriaParameters());
+    Assert.assertEquals("Broad", row.getCriteriaTypeName());
+    
+    Assert.assertEquals("2015-08-26", row.getDay());
+    Assert.assertEquals("Wednesday", row.getDayOfWeek());
+    Assert.assertEquals("Mobile devices with full browsers", row.getDevice());
+    Assert.assertEquals("https://example.com/index/type/C/", row.getEffectiveFinalUrl());
+    
+    Assert.assertEquals(3L, row.getImpressions().longValue());
+    Assert.assertEquals("2015-08-01", row.getMonth());
+    Assert.assertEquals("August", row.getMonthOfYear());
+    Assert.assertEquals("2015-07-01", row.getQuarter());
+    
+    Assert.assertEquals("Google Display Network", row.getSlot());
+    Assert.assertEquals(0.00, row.getValuePerConversionManyPerClickBigDecimal().doubleValue());
+    Assert.assertEquals("2015-08-24", row.getWeek());
+    Assert.assertEquals(2015L, row.getYear().longValue());
+  }
+
+  /**
+   * @see com.google.api.ads.adwords.awreporting.model.definitions.
+   * AbstractReportDefinitionTest#retrieveCsvEntries()
+   */
+  @Override
+  protected int retrieveCsvEntries() {
+    return 227;
+  }
+
+  /**
+   * @see com.google.api.ads.adwords.awreporting.model.definitions.
+   * AbstractReportDefinitionTest#retrievePropertiesToBeSelected()
+   */
+  @Override
+  protected String[] retrievePropertiesToBeSelected() {
+
+    return new String[] {
+        // Report
+        "ExternalCustomerId",
+        // ReportBase
+        "AccountDescriptiveName",
+        "AccountTimeZoneId",
+        "CustomerDescriptiveName",
+        "PrimaryCompanyName",
+        "AccountCurrencyCode",
+        "Date",
+        "DayOfWeek",
+        "Week",
+        "Month",
+        "MonthOfYear",
+        "Quarter",
+        "Year",
+        "Cost",
+        "Clicks",
+        "Impressions",
+        "Ctr",
+        "AverageCpm",
+        "AverageCpc",
+        "AveragePosition",
+        "Device",
+        "ClickType",
+        "AdNetworkType1",
+        "AdNetworkType2",
+        "ConversionsManyPerClick",
+        "ConversionRateManyPerClick",
+        "CostPerConversionManyPerClick",
+        "ValuePerConversionManyPerClick",
+        "ConvertedClicks",
+        "ClickConversionRate",
+        "CostPerConvertedClick",
+        "ValuePerConvertedClick",
+        "ConversionCategoryName",
+        "ConversionTypeName",
+        "ConversionValue",
+        "ViewThroughConversions",
+        // Specific to Final URL Performance Report
+        "ActiveViewCpm",
+        "ActiveViewImpressions",
+        "AdGroupId",
+        "AdGroupName",
+        "AdGroupStatus",
+        "CampaignId",
+        "CampaignName",
+        "CampaignStatus",
+        "ConversionTrackerId",
+        "CriteriaParameters",
+        "CriteriaTypeName",
+        "EffectiveFinalUrl",
+        "Slot"
+    };
+  }
+}
